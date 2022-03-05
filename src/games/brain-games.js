2be32d4637b1bacfbda80bdd992a906b17b61f8b
Vladimir Afanasiev <vladimir.afanasiev@flatstack.dev>
2022-03-05 11:45:12 +0300

    change text

--- src/games/brain-games.js
+++ src/games/brain-games.js
@@ -4,7 +4,7 @@ import readlineSync from 'readline-sync';
 const startGame = () => {
   console.log('Welcome to the Brain Games!');
   const nameUser = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${nameUser}!`);
+  console.log(`/n Hello, ${nameUser}!`);
 };
 
 export default startGame;
