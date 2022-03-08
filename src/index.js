4306634974eac814abee028f9f7793ff204dcedb
Vladimir Afanasiev <vladimir.afanasiev@flatstack.dev>
2022-03-08 16:36:40 +0300

    ss

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import readlineSync from 'readline-sync';
+import promptly from 'promptly';
 
-const numberOfRepitions = 3;
+const numberOfRepitions = [0, 1, 2];
 
-const startEngine = (gameDescription, generateGameData) => {
+const startEngine = async (gameDescription, generateGameData) => {
   console.log('Welcome to the Brain Games!');
-  const nameUser = readlineSync.question('May I have your name? ');
+  const nameUser = await promptly.prompt('May I have your name? ');
   console.log(`Hi ${nameUser}!`);
   console.log(gameDescription);
 
-  for (let index = 0; index < numberOfRepitions; index += 1) {
+  numberOfRepitions.forEach(async () => {
     const { question, answer } = generateGameData();
     console.log(`Question:  ${question}`);
-    const userAnswer = readlineSync.question('Answer: ');
+    // eslint-disable-next-line no-await-in-loop
+    const userAnswer = await promptly.prompt('Answer: ');
     if (answer !== userAnswer) {
       console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${answer}"`);
       console.log(`Let's try again, ${nameUser}!`);
       return;
     }
     console.log('Correct!');
-  }
+  });
   console.log(`Congratulations, ${nameUser}!`);
 };
 export default startEngine;
