f958f93dc1dbd7b28454c4f9057f9237f0ceb74b
Vladimir Afanasiev <vladimir.afanasiev@flatstack.dev>
2022-03-08 18:04:32 +0300

    fix prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -6,7 +6,7 @@ const isPrime = (number) => {
   if (number <= 1) {
     return false;
   }
-  for (let divisor = 2; divisor < Math.sqrt(number); divisor += 1) {
+  for (let divisor = 2; divisor <= Math.sqrt(number); divisor += 1) {
     if (number % divisor === 0) {
       return false;
     }
