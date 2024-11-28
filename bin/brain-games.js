fbab1c1abd8383f281851a3ef5bdac228636870c
Mikhail Afanasev <Miki123451@mail.ru>
2024-11-29 01:34:10 +0300

    correct

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 // eslint-disable-next-line import/extensions
-import { name } from '../src/cli.js';
+import { gatName } from '../src/cli.js';
 
-console.log(`Hello, ${name}!`);
+console.log(`Hello, ${gatName}!`);
--- src/cli.js
+++ src/cli.js
@@ -3,4 +3,4 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 
 // eslint-disable-next-line import/prefer-default-export
-export const name = readlineSync.question('May I have your name? ');
+export const gatName = readlineSync.question('May I have your name? ');
