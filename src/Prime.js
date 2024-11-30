54248659f8b2dc97486046710d65956a1e295dc4
Mikhail Afanasev <Miki123451@mail.ru>
2024-11-30 21:36:48 +0300

    correct prime.js

--- src/Prime.js
+++ src/Prime.js
@@ -15,7 +15,6 @@ const getData = () => {
       dataNumber = `${Number(getPrimeNumber[n])}`;
     }
   }
-  console.log(getTask, dataNumber);
   const questionRight = isNumberParity(getTask, dataNumber);
   return [getTask, questionRight];
 };
