fe12dbf4f6bb9cd10e687ae91060e67c6255a451
Mikhail Afanasev <Miki123451@mail.ru>
2024-11-30 22:55:25 +0300

    correct index.js

--- src/games/index.js
+++ src/games/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-plusplus */
-/* eslint-disable import/named */
 /* eslint-disable import/extensions */
 import readlineSync from 'readline-sync';
 import { getName } from './..greeting.js';
