49f4be6e3d3a9cc3c37a8d8f78c7b4a9a9830e50
Mikhail Afanasev <Miki123451@mail.ru>
2024-11-30 22:59:58 +0300

    +

--- src/greeting.js
+++ src/greeting.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/prefer-default-export */
-// eslint-disable-next-line import/extensions
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
