a795248a9d65aad59d0898f689a5f6e05f40ced8
Mikhail Afanasev <Miki123451@mail.ru>
2024-12-01 23:13:50 +0300

    +

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,5 @@
-// eslint-disable-next-line import/no-unresolved
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
 
-// eslint-disable-next-line import/prefer-default-export
 export const gatName = readlineSync.question('May I have your name? ');
