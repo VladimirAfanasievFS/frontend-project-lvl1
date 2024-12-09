d4e1d782ea6f3e4fc315a6112e0f97d0aad8804c
Mikhail Afanasev <Miki123451@mail.ru>
2024-12-09 17:05:35 +0300

    functions

--- src/prime.js
+++ src/prime.js
@@ -2,18 +2,21 @@ import { getRandomNumber, storesPrimeNumber } from './const.js';
 import launchLogic from './games/index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const isNumberParity = (num1, num2) => (num1 === Number(num2) ? 'yes' : 'no'); // проверяем на четность число
-
-const getData = () => {
-  let dataNumber = 0;
-  const question = getRandomNumber(1, 99);
-  const getPrimeNumber = storesPrimeNumber();// простые числа
-  for (let n = 0; n < getPrimeNumber.length; n += 1) {
-    if (Number(getPrimeNumber[n]) === Number(question)) {
-      dataNumber = `${Number(getPrimeNumber[n])}`;
+const isNumberSimple = (num) => (num === true ? 'yes' : 'no'); // проверяем на четность число
+const getNumberSimple = (number) => {
+  if (number <= 1) return false;
+  if (number === 2) return true;
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
     }
   }
-  const questionRight = isNumberParity(question, dataNumber);
+  return true;
+};
+
+const getData = () => {
+  const question = getRandomNumber(1, 99);
+  const questionRight = isNumberSimple(getNumberSimple(question));
   return [question, questionRight];
 };
 
