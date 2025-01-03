765a63840e517d8666bcbfd3689c2274be1be8d0
Mikhail Afanasev <Miki123451@mail.ru>
2025-01-03 17:33:40 +0300

    correct length line

--- src/games/progression.js
+++ src/games/progression.js
@@ -16,7 +16,6 @@ const numberGeneration = (lengthProgression, arithmeticRandom) => {
   }
   return arr;
 };
-
 const generateMissingProgressin = (number) => {
   const numberRandom = getRandomNumber(1, (number.length - 1));
   let a = 0;
@@ -35,47 +34,17 @@ const generateMissingProgressin = (number) => {
   // reply = reply.slice(0, -1);
   return [resul, reply];
 };
-
-
-// const getExpression = (number, numberLength, arithmetic, secretNumber) => {
-//   let i = 0;
-//   let missNumber = 0;
-//   let a = 0;
-//   let sum = number;
-//   let result = '';
-//   while (i <= numberLength) {
-//     sum += arithmetic;
-//     if (i === secretNumber) {
-//       a = sum;
-//       missNumber = a;
-//       a = '..';
-//       result = `${result}${a} `;
-//     } else {
-//       result = `${result}${sum} `;
-//     }
-//     i += 1;
-//   }
-//   const question = result;
-//   const answer = String(missNumber);
-//   return [question, answer];
-// };
 const generateRound = () => {
   const lengthProgression = getRandomNumber(5, 10);
   const arithmeticRandom = getRandomNumber(1, 3);
-  const [resul, reply] = generateMissingProgressin(numberGeneration(lengthProgression, arithmeticRandom));
+  const [resul, reply] = generateMissingProgressin(
+    numberGeneration(lengthProgression, arithmeticRandom),
+  );
   const question = String(resul);
   const answer = String(reply);
   return [question, answer];
 };
 
-// const generateRound = () => {
-//   const number1 = getRandomNumber(1, 20);
-//   const lengthProgression = getRandomNumber(5, 10);
-//   const arithmeticRandom = getRandomNumber(1, 4);
-//   const getSecretNumber = getRandomNumber(1, lengthProgression);
-//   return getExpression(number1, getSecretNumber, lengthProgression, arithmeticRandom);
-// };
-// console.log(generateRound(numberGeneration()));
 const start = () => launchLogic(description, generateRound);
 
 export { description, start };
