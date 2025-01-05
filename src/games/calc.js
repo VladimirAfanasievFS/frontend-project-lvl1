b3642974e3609661002e0a46316742ca62f6a888
Mikhail Afanasev <Miki123451@mail.ru>
2025-01-05 13:59:22 +0300

    correct calc.js line 20

--- src/games/calc.js
+++ src/games/calc.js
@@ -17,7 +17,7 @@ const getExpression = (sign, number1, number2) => {
 const generateRound = () => {
   const number1 = getRandomNumber(1, 99);
   const number2 = getRandomNumber(1, 99);
-  const numberSign = getRandomNumber(0, 2);
+  const numberSign = getRandomNumber(0, (getSign.length));
   const sing = getSign[numberSign];
   const question = `${number1} ${sing} ${number2}`;
   const answer = String(getExpression(sing, number1, number2));
