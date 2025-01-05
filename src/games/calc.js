87c773fea0810ad11b537a157665436d53cd20ea
Mikhail Afanasev <Miki123451@mail.ru>
2025-01-05 14:09:21 +0300

    correct calc.js, line 20 add '-1'

--- src/games/calc.js
+++ src/games/calc.js
@@ -17,7 +17,7 @@ const getExpression = (sign, number1, number2) => {
 const generateRound = () => {
   const number1 = getRandomNumber(1, 99);
   const number2 = getRandomNumber(1, 99);
-  const numberSign = getRandomNumber(0, (getSign.length));
+  const numberSign = getRandomNumber(0, (getSign.length - 1));
   const sing = getSign[numberSign];
   const question = `${number1} ${sing} ${number2}`;
   const answer = String(getExpression(sing, number1, number2));
