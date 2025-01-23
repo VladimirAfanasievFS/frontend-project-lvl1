7aa6bf6e1c0320e7128247b7b52f6fa27ebb9901
Mikhail Afanasev <Miki123451@mail.ru>
2025-01-23 21:55:24 +0300

    file calc.js correct getSigns on operators and getExpression on calculate

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,8 +2,8 @@ import getRandomNumber from '../helpers.js';
 import launchLogic from '../index.js';
 
 const description = 'What is the result of the expression?';
-const getSigns = ['*', '-', '+'];
-const getExpression = (sign, number1, number2) => {
+const operators = ['*', '-', '+'];
+const calculate = (sign, number1, number2) => {
   switch (sign) {
     case '*':
       return number1 * number2;
@@ -17,10 +17,10 @@ const getExpression = (sign, number1, number2) => {
 const generateRound = () => {
   const number1 = getRandomNumber(1, 99);
   const number2 = getRandomNumber(1, 99);
-  const numberSign = getRandomNumber(0, (getSigns.length - 1));
-  const sing = getSigns[numberSign];
+  const numberSign = getRandomNumber(0, (operators.length - 1));
+  const sing = operators[numberSign];
   const question = `${number1} ${sing} ${number2}`;
-  const answer = String(getExpression(sing, number1, number2));
+  const answer = String(calculate(sing, number1, number2));
   return [question, answer];
 };
 
