cc29f386aa638890db3371de6058b84bec54d05f
Mikhail Afanasev <Miki123451@mail.ru>
2025-01-29 21:04:00 +0300

    correct even.js and prime.js

--- src/games/even.js
+++ src/games/even.js
@@ -2,11 +2,11 @@ import getRandomNumber from '../helpers.js';
 import launchLogic from '../index.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
-const isEven = (num) => (num % 2 === 0 ? 'yes' : 'no');
+const isEven = (num) => (num % 2 === 0);
 
 const generateRound = () => {
   const question = getRandomNumber(1, 99);
-  const rightAnswer = isEven(question);
+  const rightAnswer = isEven(question) ? 'yes' : 'no';
   return [question, rightAnswer];
 };
 
--- src/games/prime.js
+++ src/games/prime.js
@@ -2,7 +2,7 @@ import getRandomNumber from '../helpers.js';
 import launchLogic from '../index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const isPrime = (num) => (num === true ? 'yes' : 'no');
+const isPrime = (num) => (num === true);
 const getNumberSimple = (number) => {
   if (number <= 1) return false;
   if (number === 2) return true;
@@ -16,7 +16,7 @@ const getNumberSimple = (number) => {
 
 const generateRound = () => {
   const question = getRandomNumber(1, 99);
-  const rightAnswer = isPrime(getNumberSimple(question));
+  const rightAnswer = isPrime(getNumberSimple(question)) ? 'yes' : 'no';
   return [question, rightAnswer];
 };
 
