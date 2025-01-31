fd4bcf9019532575ad6e2fb33ac1554a15b5e79b
Mikhail Afanasev <Miki123451@mail.ru>
2025-01-31 20:56:12 +0300

    delete greeting.js, data transfer to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
-import greeting from './greeting.js';
 
 const launchLogic = (description, generateRound) => {
-  const [userName] = greeting();
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   console.log(`${description}`);
   const roundsCount = 3;
   for (let i = 1; i <= roundsCount; i += 1) {
