40e92ecd937bb1c0451b926277784b866ecdf998
Mikhail Afanasev <Miki123451@mail.ru>
2025-01-31 22:00:15 +0300

    file prime.js, correct const isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,7 +2,7 @@ import getRandomNumber from '../helpers.js';
 import launchLogic from '../index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const isPrime = (num) => (num === true);
+const isPrime = (num) => (num === true ? 'yes' : 'no');
 const getNumberSimple = (number) => {
   if (number <= 1) return false;
   if (number === 2) return true;
@@ -13,10 +13,10 @@ const getNumberSimple = (number) => {
   }
   return true;
 };
-
 const generateRound = () => {
   const question = getRandomNumber(1, 99);
-  const answer = isPrime(getNumberSimple(question)) ? 'yes' : 'no';
+
+  const answer = isPrime(getNumberSimple(question));
   return [question, answer];
 };
 
